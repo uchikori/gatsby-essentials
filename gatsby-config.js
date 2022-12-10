c61cc70b92f65d3b7ab0fb559ed6dd6b1df5cfc1
uchikori <ucchii.wa@gmail.com>
2022-12-10 18:41:13 +0900

    画像最適化のプラグインを導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,16 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      }
+    },
+  ],
 }
