660091fb8e538734512c23fca2acd51fb5c73fa8
uchikori <ucchii.wa@gmail.com>
2022-12-18 12:02:09 +0900

    ♻️ FontAwesomeの読み込み時の巨大化防ぐ

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Header } from "./header";
 import { Footer } from "./footer";
 import "./layout.css";
-
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddcss = false;
 
 export const Layout = (props) => {
     const {children} = props;
