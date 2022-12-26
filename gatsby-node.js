0e5e18f2f99f128c1d3cf946640073308a7a1dca
uchikori <ucchii.wa@gmail.com>
2022-12-26 15:42:38 +0900

    カテゴリーページの生成

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,6 +21,14 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
                     }
                 }
             }
+            allContentfulCategory {
+                edges {
+                    node{
+                        categorySlug
+                        id
+                    }
+                }
+            }
         }
     `)
 
@@ -29,6 +37,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         return
     }
 
+    //記事一覧ページの生成
     blogresult.data.allContentfulBlogPost.edges.forEach((edge) => {
         createPage({
             path: `/blog/post/${edge.node.slug}`,
@@ -46,19 +55,33 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     const blogPosts = blogresult.data.allContentfulBlogPost.edges.length;
     const pages = Math.ceil(blogPosts / blogPostPerPage);
 
-    console.log(pages);
-
     const array = Array.from({ length:pages });
     array.forEach((item, index) => {
         createPage({
             path: index === 0 ? `/blog/` : `blog/post/${index + 1}`,
             component: path.resolve(`./src/templates/blog-template.js`),
             context: {
-                skip: blogPostPerPage * index,
-                limit: blogPostPerPage,
-                currentPage: index + 1,
-                isFirst: index + 1 === 1,
-                isLast: index + 1 === pages
+                skip: blogPostPerPage * index, //0番目の記事からスキップする記事の個数
+                limit: blogPostPerPage,        //1ページ内に表示する記事数
+                currentPage: index + 1,        //現在のページ
+                isFirst: index + 1 === 1,      //最初のページかどうか
+                isLast: index + 1 === pages,   //最後のページかどうか
+            }
+        })
+    })
+
+    //カテゴリーページの生成
+    blogresult.data.allContentfulCategory.edges.forEach((edge) => {
+        createPage({
+            path: `/cat/${edge.node.categorySlug}`,
+            component: path.resolve(`./src/templates/category-template.js`),
+            context: {
+                catId: edge.node.id,
+                skip: 0,        //0番目の記事からスキップする記事の個数
+                limit: 100,     //1ページ内に表示する記事数
+                currentPage: 1, //現在のページ
+                isFirst: true,  //最初のページかどうか
+                isLast: true,   //最後のページかどうか 
             }
         })
     })
---
+++ src/templates/category-template.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Layout } from "../components/Layout";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { Seo } from "../components/seo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+export default function blog(props) {
+    const { data, pageContext, location} = props;
+
+    return(
+        <>
+        <Seo
+        pagetitle="ブログ"
+        pagedesc="ESSENTIALSのブログです"
+        pagepath={location.pathname}
+        pageimg={null}
+        />
+        <Layout>
+            <section className="content bloglist">
+                <div className="container">
+                    <h1 className="bar">RECENT POSTS</h1>
+
+                    <div className="posts">
+
+                        {data.allContentfulBlogPost.edges.map(({ node }) => {
+                            return (
+                                <article className="post" key={ node.id }>
+                                    <Link to={`/blog/post/${node.slug}`}>
+                                        <figure>
+                                            <GatsbyImage 
+                                            image={node.eyecatch.gatsbyImageData} 
+                                            alt={node.eyecatch.description} 
+                                            style={{ height: "100%" }}
+                                            />
+                                        </figure>
+                                        <h3>{node.title}</h3>
+
+                                    </Link>
+                                </article>
+                            )
+                        })}
+
+                    </div>
+
+                    <ul className="pagenation">
+                        {!pageContext.isFirst && (
+                            <li className="prev">
+                                <Link 
+                                to={pageContext.currentPage === 2 ? `/blog/` : `/blog/post/${pageContext.currentPage - 1}`} 
+                                rel="prev">
+                                    <FontAwesomeIcon icon={faChevronLeft} />
+                                    <span>前のページ</span>
+                                </Link>
+                            </li>
+                        )}
+
+                        {!pageContext.isLast && (
+                            <li className="next">
+                                <Link to={`/blog/post/${pageContext.currentPage + 1}`} rel="next">
+                                    <span>次のページ</span>
+                                    <FontAwesomeIcon icon={faChevronRight} />
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+
+                </div>
+            </section>
+        </Layout>
+        </>
+    )
+}
+
+export const query = graphql`
+query($catId: String!, $skip: Int!, $limit: Int!) {
+    allContentfulBlogPost(
+        sort: {publishDate: DESC}, 
+        skip:$skip, 
+        limit:$limit,
+        filter: {category: {elemMatch: {id: {eq: $catId}}}}
+    ) {
+        edges {
+            node {
+                title
+                id
+                eyecatch {
+                    gatsbyImageData(
+                        width: 573, 
+                        layout: CONSTRAINED,
+                        sizes: "(min-width: 1146px) 573px, 50vw"
+                    )
+                    description
+                }
+                slug
+            }
+        }
+    }
+}
+`
